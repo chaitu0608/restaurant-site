7ab3140e5f693edf66f13791deb6f9e20bf7eb8d
Chaitanya Dhamdhere <chaitu0608@gmail.com>
2025-01-09 12:25:44 +0530

    navbar open button fix and js navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,29 @@ window.addEventListener("load",function(){
     preloader.classList.add("loaded");
     document.body.classList.add("loaded");
 });
+
+
+/* Add Event Listener for Multiple Elements */
+const addEventOnElements = function (elements, eventType, callback) {
+    // Loop through all elements in the NodeList 'elements'
+    for (let i = 0, len = elements.length; i < len; i++) {
+        // Add the specified event listener to each element
+        elements[i].addEventListener(eventType, callback);
+    }
+};
+
+
+
+/* NAVBAR*/
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+
+const toggleNavbar=function(){
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classKist.toggle("nav=active");
+}
+addEventOnElements(navTogglers, "click", toggleNavbar);
